11cbda8e87c452a8bf4917108e1add118983fc3e
root <stardust@sponsus.org>
2021-01-31 04:03:19 +0000

    Fix NPM not found bug

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 
 module.exports = {
     context: __dirname,
-    entry: './example.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'cfw-easy-html.js',
