5581730fd772ae5186472dcb9f934db9d71c291d
root <stardust@sponsus.org>
2021-01-31 04:15:05 +0000

    Make WBPK target Node during the build

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ var path = require('path')
 module.exports = {
     context: __dirname,
     entry: './src/index.js',
+    target: 'node',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'cfw-easy-html.js',
-        library: 'cfw-easy-html',
-        libraryTarget: 'umd',
+        library: 'cfw-easy-html'
     },
     module: {
         rules: [
